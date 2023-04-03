626af27f1cbf87c2fb8b99ca62d0f3b3922a4cf5
김민석|Minseok Kim <alstjr7437>
2023-04-03 14:05:40 +0900

    first commit

---
+++ week1.cpp
@@ -0,0 +1,51 @@
+#include <windows.h>
+
+#include <gl\glut.h>
+
+#include <gl\gl.h>
+
+#include <gl\glu.h>
+
+
+
+void MyDisplay() {
+
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glBegin(GL_POLYGON);
+
+    glVertex3f(-0.5, -0.5, 0.0);
+
+    glVertex3f(0.5, -0.5, 0.0);
+
+    glVertex3f(0.5, 0.5, 0.0);
+
+    glVertex3f(-0.5, 0.5, 0.0);
+
+    glEnd();
+
+    glFlush();
+
+}
+
+
+
+int main() {
+
+    glutInitWindowPosition(100, 100);
+
+    glutInitWindowSize(400, 500);
+
+    glutInitDisplayMode(GLUT_RGBA);
+
+    glutCreateWindow("OpenGL Drawing Example");
+
+
+
+    glutDisplayFunc(MyDisplay);
+
+    glutMainLoop();
+
+    return 0;
+
+}
